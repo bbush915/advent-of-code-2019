b99e38089d42a29e6136dcfcf6d8e8ee386d66c0
Bryan Bush <bryan@dialexa.com>
2021-11-29 17:07:36 -0600

    Updates.

---
+++ README.md
@@ -0,0 +1,3 @@
+# advent-of-code-2019
+
+Advent of Code is an Advent calendar of small programming puzzles for a variety of skill sets and skill levels.
--- utils.ts
+++
@@ -1,7 +0,0 @@
-export function time(func: () => any) {
-  const start = new Date().getTime();
-
-  const result = func();
-
-  return { elapsed: new Date().getTime() - start, result };
-}
